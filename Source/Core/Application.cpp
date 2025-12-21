439c95694602d6bed79b43310afbe5acc8adcb55
Adam Aske <adamaske@live.no>
2025-12-21 19:22:53 +0100

    Remove ITK and MRISystem integration

--- Source/Core/Application.cpp
+++ Source/Core/Application.cpp
@@ -58,7 +58,6 @@ Application::Application(const ApplicationSpecification& spec) : specification_(
 	auto wings_system = system_manager_.AddSystem<WingsPlottingSystem>();
 	auto control_panel_system_ = system_manager_.AddSystem<ControlPanelSystem>(*this);
 
-	auto mri_system = system_manager_.AddSystem<MRISystem>();
 	// Register
 	plotting_system->RegisterProjectionTimeSubscriber(projection_system);
 
--- Source/Systems/SystemManager.h
+++ Source/Systems/SystemManager.h
@@ -93,5 +93,3 @@ private:
 #include "Systems/ImGuiSystem.h"
 #include "Systems/WingsPlottingSystem.h"
 #include "Systems/ControlPanelSystem.h"
-#include "Systems/MRISystem.h"
-
